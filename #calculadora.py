976a0da08ec5e17fca03efe3a11e50071e2fa1e9
Thougzaz191 <CETECOM@AVAWSV01LC0804.duoc.acad>
2025-06-03 12:27:02 -0400

    hola

--- #calculadora.py
+++ #calculadora.py
@@ -6,13 +6,11 @@ print ("2.RESTA")
 op = int(input("Elige una opcion"))
 
 def suma (a,b):
-    if op == "1":
-
+    if op == "1": 
         resultado = a + b
         return resultado
     elif op == "2":
         resultado = a - b
         return resultado
-
 
 print(suma(a,b))
