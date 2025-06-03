e986667f00c0b315b8708fa260af24a271ab9d0e
Thougzaz191 <CETECOM@AVAWSV01LC0804.duoc.acad>
2025-06-03 12:15:25 -0400

    nose

--- #calculadora.py
+++ #calculadora.py
@@ -1,10 +1,18 @@
 #calculadora
 a = int(input(""))
 b = int(input(""))
+print ("1.SUMA")
+print ("2.RESTA")
+op = int(input("Elige una opcion"))
 
 def suma (a,b):
-    resultado = a + b
-    return resultado
+    if op == "1":
+
+        resultado = a + b
+        return resultado
+    elif op == "2":
+        resultado = a - b
+        return resultado
 
 
 print(suma(a,b))
